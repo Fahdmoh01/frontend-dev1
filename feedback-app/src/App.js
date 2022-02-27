50ec7b519d313c2b7720684cea2c63a4c2fa74f8
Fahd Mohammed <fahdmoh.1@gmail.com>
2022-02-27 12:00:46 +0000

    beginning to like this thing

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,29 +1,32 @@
 //Global states are set and manipulated in App.js
-import { useState } from "react";
-import Header from "./components/Header";
-import FeedbackList from "./components/FeedbackList";
-import FeedBackData from "./data/FeedBackData";
-import FeedbackStats from "./components/FeedbackStats";
-import FeedbackForm from "./components/FeedbackForm";
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedBackData from './data/FeedBackData'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 const App = () => {
-  const [feedback, setFeedback] = useState(FeedBackData);
+    const [feedback, setFeedback] = useState(FeedBackData)
 
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
     }
-  };
 
-  return (
-    <>
-      <Header />
-      <div className="container">
-		  <FeedbackForm />
-		  <FeedbackStats feedback={feedback}/>
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
-    </>
-  );
-};
-export default App;
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <FeedbackForm />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                />
+            </div>
+        </>
+    )
+}
+export default App
--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,31 +1,35 @@
-import PropTypes from "prop-types";
-import FeedbackItem from "./FeedbackItem";
+import PropTypes from 'prop-types'
+import FeedbackItem from './FeedbackItem'
 
 function FeedbackList({ feedback, handleDelete }) {
-  if (!feedback || feedback.length === 0) {
-    return <div>No FeedBack yet</div>;
-  }
+    if (!feedback || feedback.length === 0) {
+        return <div>No FeedBack yet</div>
+    }
 
-  return (
-    <div className="feedback-list">
-      {feedback.map((item) => {
-        return (
-          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="feedback-list">
+            {feedback.map((item) => {
+                return (
+                    <FeedbackItem
+                        key={item.id}
+                        item={item}
+                        handleDelete={handleDelete}
+                    />
+                )
+            })}
+        </div>
+    )
 }
 
 //it's not necessary to shape Array but doing it anyway
 FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  ),
-};
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
+    ),
+}
 
-export default FeedbackList;
+export default FeedbackList
